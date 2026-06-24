d1df9e3afe8c0551d02503f26c294a0eca308f9c
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2026-06-24 17:02:38 +0200

    feat(app): line_items SeaORM entity

---
+++ app/src/models/entities/line_items.rs
@@ -0,0 +1,18 @@
+use ferro::FerroModel;
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize, FerroModel)]
+#[sea_orm(table_name = "line_items")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub order_id: i64,
+    pub amount: f64,
+    pub tenant_id: i64,
+    #[sea_orm(column_name = "deleted_at")]
+    pub deleted_at: Option<String>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
